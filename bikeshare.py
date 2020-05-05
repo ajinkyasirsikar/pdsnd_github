cbdb2c77fb4ac9566a66644e7305e54b24578d52
ajinkyasirsikar <ajinkyasirsikar@gmail.com>
2020-05-05 09:31:54 -0700

    Title: Added main title for the project

--- bikeshare.py
+++ bikeshare.py
@@ -15,6 +15,9 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    title="US Bikeshare data"
+    print("This project is called as:{}".format(title))
+
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
